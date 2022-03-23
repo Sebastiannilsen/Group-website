5c889abe1a07766eb19b4d8b09f563ec2071f85d
Sebastian Nilsen <sebast@stud.ntnu.no>
2022-03-23 12:37:59 +0100

    Minor tweaks to navBar

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,16 +1,37 @@
-window.onload = function (){
+
+function addAndRemoveClassWhenScrolledOnTopOfPage(){
     let navHeader = document.getElementById("header");
     window.onscroll = function() {
 
 
-        // @var int scrollPoint
+        // @let int scrollPoint
         let scrollPoint = window.scrollY;
 
-        // check if we hit the bottom of the page
+        // check if we are under the top of the page
         if (scrollPoint > 0) {
             navHeader.classList.add("isScrolled");
         } else {
             navHeader.classList.remove("isScrolled");
         }
     }
+}
+
+function addWhiteBackgroundToNavWhenHamburgerIsHovered(){
+    let navHeader = document.getElementById("header");
+    let dropdownContent = document.getElementById("dropDownOnHover");
+
+    dropdownContent.addEventListener("mouseover", function (){
+        navHeader.classList.add("isScrolledEmmidietly");
+    })
+
+    dropdownContent.addEventListener("mouseleave", function (){
+        navHeader.classList.remove("isScrolledEmmidietly");
+    })
+}
+
+
+
+window.onload = function (){
+    addAndRemoveClassWhenScrolledOnTopOfPage();
+    addWhiteBackgroundToNavWhenHamburgerIsHovered();
 }
